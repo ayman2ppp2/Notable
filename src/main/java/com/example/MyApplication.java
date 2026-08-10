632eafb40f47f1f2e367c9c214bd2aeddd7198ef
ayman2ppp2 <whiteshadow2ppp2@gmail.com>
2026-08-10 11:01:25 +0200

    initial commit

---
+++ src/main/java/com/example/MyApplication.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MyApplication {
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String home() {
+        return "hello can you hear me";
+    }
+
+    public static void main(String[] args) {
+       
+        
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
